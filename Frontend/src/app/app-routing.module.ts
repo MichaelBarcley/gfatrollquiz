56c5073649eebca5b043e97945597308d5a83b7d
Mihály Barczi <barczi.mihaly@gmail.com>
2019-03-06 21:16:59 +0100

    add blank components and routing

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { GameComponent } from './components/game/game.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { LeaderboardComponent } from './components/leaderboard/leaderboard.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { NewQuestionComponent } from './components/new-question/new-question.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingComponent},
+  { path: 'game', component: GameComponent},
+  { path: 'leaderboard', component: LeaderboardComponent},
+  { path: 'newquestion', component: NewQuestionComponent},
+  { path: 'admin', component: AdminComponent},
+  { path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { GameComponent } from './components/game/game.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { LeaderboardComponent } from './components/leaderboard/leaderboard.component';
+import { NewQuestionComponent } from './components/new-question/new-question.component';
+import { AdminComponent } from './components/admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent,
+    GameComponent,
+    LandingComponent,
+    LeaderboardComponent,
+    NewQuestionComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
